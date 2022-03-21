8ab75106dee5a63d6ecbeffe06c04e88fc909884
Muhammad Talha <talhashabbir9@gmail.com>
2022-03-21 22:27:33 +0500

    user seeder with one user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       $user = [
+        'name' => 'Admin',
+        'email' => 'admin@laravel.com',
+        'password' => Hash::make('password'),
+        'date_of_birth' => '1990-10-09'
+       ];
+       
+       User::create($user);
+    }
+}
