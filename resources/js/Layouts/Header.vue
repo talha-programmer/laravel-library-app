36c86e07e63e002511e4800abe16fb002b19e469
Muhammad Talha <talhashabbir9@gmail.com>
2022-03-21 22:22:02 +0500

    components copied from Tailmin template

---
+++ resources/js/Layouts/Header.vue
@@ -0,0 +1,86 @@
+<script setup>
+import BreezeDropdownLink from "@/Components/DropdownLink.vue";
+</script>
+
+<template>
+  <header
+    class="flex justify-between bg-white border-b-2 px-4 sm:justify-end sm:px-6"
+  >
+    <button class="sm:hidden">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 text-gray-600"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg>
+    </button>
+    <Menu as="div" class="relative">
+      <MenuButton
+        class="flex items-center space-x-2 px-2 py-3 text-sm hover:bg-gray-200 focus:outline-none"
+      >
+        <span>
+          {{ $page.props.auth.user.name }}
+        </span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-4 text-gray-600"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </MenuButton>
+      <transition
+        enter-active-class="transition ease-out duration-100"
+        enter-from-class="transform opacity-0 scale-95"
+        enter-to-class="transform opacity-100 scale-100"
+        leave-active-class="transition ease-in duration-75"
+        leave-from-class="transform opacity-100 scale-100"
+        leave-to-class="transform opacity-0 scale-95"
+      >
+        <MenuItems
+          class="origin-top-right absolute right-0 w-48 shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-200 focus:outline-none"
+        >
+          <div>
+            <MenuItem>
+              <BreezeDropdownLink
+                :href="route('logout')"
+                method="post"
+                as="button"
+              >
+                Log Out
+              </BreezeDropdownLink>
+            </MenuItem>
+          </div>
+        </MenuItems>
+      </transition>
+    </Menu>
+  </header>
+</template>
+
+<script>
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+
+export default {
+  components: {
+    Menu,
+    MenuButton,
+    MenuItems,
+    MenuItem
+  }
+};
+</script>
---
+++ resources/js/Layouts/Sidebar.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+</script>
+<template>
+  <aside class="hidden w-64 bg-gray-800 sm:block">
+    <div
+      class="py-3 text-2xl uppercase text-center tracking-widest bg-gray-900 border-b-2 border-gray-800 mb-8"
+    >
+      <Link :href="route('dashboard')" class="text-white">Library App</Link>
+    </div>
+
+    <nav class="text-sm text-gray-300">
+      <ul class="flex flex-col">
+        <li class="px-4 cursor-pointer hover:bg-gray-700">
+          <Link :href="route('books')" class="text-white py-1 flex items-center">All Books</Link>
+        </li>
+        <li class="px-4 cursor-pointer hover:bg-gray-700">
+          <Link :href="route('books.form')" class="text-white py-1 flex items-center">New Book</Link>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+</template>
