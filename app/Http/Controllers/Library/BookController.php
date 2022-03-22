3d19af479d558e654fbbe8a6c89f3d0db5fc8053
Muhammad Talha <talhashabbir9@gmail.com>
2022-03-22 23:33:29 +0500

    checkin and checkout methods

--- app/Http/Controllers/Library/BookController.php
+++ app/Http/Controllers/Library/BookController.php
@@ -51,7 +51,7 @@ class BookController extends Controller
 
             if ($isSaved) {
                 return redirect(route('books'))
-                ->with('status', 'Book saved successfully.');
+                    ->with('status', 'Book saved successfully.');
             } else {
                 return response()->json(['error']);
             }
@@ -62,9 +62,39 @@ class BookController extends Controller
     {
         $isDeleted = $this->bookService->destroy($book);
         if ($isDeleted) {
-            return response()->json(['success']);
+            return back()->with('success', "Book deleted.");
         } else {
-            return response()->json(['error']);
+            return back()->with('error', "Error occurred while deleting book.");
         }
+    }
+
+    public function checkin(Request $request, Book $book)
+    {
+        $isCheckedOut = $this->bookService->isCheckedOut($book);
+
+        if (!$isCheckedOut) {
+            return back()->with('error', 'Book already checked in.');
+        }
+
+        $checkin = $this->bookService->checkin($book);
+        if ($checkin) {
+            return redirect(route('books'))->with('success', 'Checkin successfull.');
+        }
+        return back()->with('error', 'Error occurred while checkin.');
+    }
+
+    public function checkout(Request $request, Book $book)
+    {
+        $isAvailable = $this->bookService->isAvailable($book);
+
+        if (!$isAvailable) {
+            return back()->with('error', 'Book already checked out.');
+        }
+
+        $checkout = $this->bookService->checkout($book);
+        if ($checkout) {
+            return redirect(route('books'))->with('success', 'Checkout successfull.');
+        }
+        return back()->with('error', 'Error occurred while checkout.');
     }
 }
